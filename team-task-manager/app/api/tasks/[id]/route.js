5b290415c2e447cf9a785bb929bb38f01508c32b
Nikhil-Kaul2003 <nikhilkaul03@gmail.com>
2026-05-07 14:42:03 +0530

    fixed prisma import

--- team-task-manager/app/api/tasks/[id]/route.js
+++ team-task-manager/app/api/tasks/[id]/route.js
@@ -1,5 +1,5 @@
 import { NextResponse } from "next/server";
-import prisma from "@/lib/prisma";
+import { prisma } from "../../../../lib/prisma";
 
 // UPDATE TASK
 export async function PUT(req, { params }) {
